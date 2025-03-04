c6c1dfa2a5c2ee6741f9f0ca25fc96b9f57a4fd6
Nicholas Brites <nickbrites1@gmail.com>
2025-03-04 16:51:25 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,15 +11,11 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "books");
-		
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21, "bob", "bob@gmail", new Date(), 3000.00, obj);
-
-		System.out.println(seller);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+	
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
